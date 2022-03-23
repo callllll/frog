31613942fc82c339365caaae5a3ea83bde1e2fb2
callllll <91920400+callllll@users.noreply.github.com>
2022-03-22 22:16:33 -0700

    pics

--- frog.js
+++ frog.js
@@ -1,13 +1,49 @@
-let frogs = ["frog1", "frog2", "frog3", "frog4"]
+let frogs = [{name: "frog1"},
+ {name: "frog2"},
+ {name: "frog3"},
+ {name: "frog4"},
+ {name: "frog5"},
+ {name: "frog6"},
+ {name: "frog7"},
+ {name: "frog8"},
+ {name: "frog9"},
+ {name: "frog10"}];
+
+let randomizer
+let animation = false;
+let frogpics = [];
+
+function preload(){
+
+    for (let i = 0; i <= 10; i++){
+        frogpics[i] = loadImage("assets/frogs" + i + ".JPG")
+    }
+}
 
 function setup(){
     createCanvas(600,600);
     background(200);
+    textSize(20);
+    imageMode(CENTER);
 
-    console.log(frogs[int(random(5))]);
+    text("click for good vibes!", 50, 50);
 
+    
 }
 
 function draw(){
 
+    if(animation == true){
+        image(frogs[0], width/2, height/2);
+    }
+}
+
+function randomizer(){
+    animation = false;
+    if (frogs[0]){
+        background(random(200,255));
+        randomIndex = int(random(frogs.length));
+        text(frogs[randomIndex].normalize, 50, 50);
+        frogs.splice(randomIndex, 1);
+    }
 }
