22116db71ef0af39cf1f518d64c0c0658fb6aef0
callllll <91920400+callllll@users.noreply.github.com>
2022-03-23 19:14:41 -0700

    final code

--- frog.js
+++ frog.js
@@ -1,60 +1,51 @@
-let quotes = ["Failure is the condiment that gives success its flavor.", "You are enough just as you are.", "It might not be easy but it'll be worth it", 
-"Everything happens for a reason", "What is yours will find you.", "Take the risk or lose the chance.", "Things get better.", 
-"Take things one step at a time.", "Give yourself some credit for how far you've come.", "It's ok to not be ok. Just don't give up."]
-let frogs = []
-let button;
-
-let frog1 = loadImage("frog1.jpg");
-let frog2 = loadImage("frog2.jpg");
-let frog3 = loadImage("frog3.jpg");
-let frog4 = loadImage("frog4.jpg");
-let frog5 = loadImage("frog5.jpg");
-let frog6 = loadImage("frog6.jpg");
-let frog7 = loadImage("frog7.jpg");
-let frog8 = loadImage("frog8.jpg");
-let frog9 = loadImage("frog9.jpg");
-let frog10 = loadImage("frog10.jpg");
-
-frogs = [frog1,frog2,frog3,frog4,frog5,frog6,frog7,frog8,frog9,frog10];
+let frogQuote = [
+    {name:"Failure is the condiment that gives success its flavor."}, 
+    {name:"You are enough just as you are."}, 
+    {name:"It might not be easy but it'll be worth it"}, 
+    {name:"Everything happens for a reason"}, 
+    {name:"What is yours will find you."}, 
+    {name:"Take the risk or lose the chance."}, 
+    {name:"Things get better."}, 
+    {name:"Take things one step at a time."}, 
+    {name:"Give yourself some credit for how far you've come."}, 
+    {name:"It's ok to not be ok. Just don't give up."},
+    {name:"You are the main protagonist of your own story."},
+    {name:"There's no advantage to hurring through life."},
+    {name:"The human mind works in mysterious ways. You just need to see the sun shining in the morning to feel energized. A blue sky makes you feel happy you're alive."},
+    {name:"Even if they have wings, birds can never fly without a sky."},
+    {name:"Whatever happens, happens."}];
 
 
-function setup(){
-    createCanvas(600, 600);
-    background(220);
+    let index = 0;
+let buttonPressed = false;
 
-    let button = createButton("Click to be happy.");
-    button.style('color', 'rgb(0,150,200)');
-    button.style('backgroundColor', 'rgb(0,255,0)');
-    button.style('borderColor', 'rgb(250,255,0)');
-    button.style('fontSize', 'large');
-    button.position(235,500);
 
-}
 
-function draw(){
-    textSize(70);
-    fill(0)
-    textAlign(CENTER);
-    textFont(arial);
+
+function setup() {
+  createCanvas(1845, 975);
+  background(255,253,208); 
+  textSize(20);
+  text("Click for some advice", 10,30);
+  console.log(frogQuote[index]);
+  //index =int(random(5));
+  console.log(index);
+  mouseClicked(buttonPressed);
+
 
 }
-
-function mousePressed(){
-    background(color(random(255,253,208), random(237,231,221)));
-    fill("#ffffff");
-    square(125, 125, 350, 20);
-    imageMode(CENTER);
-    let randomFrog = random(frogs);
-    randomFrog.resize(300,300);
-    image(randomFrog, width/2, height/2);
-
-    randomQuote();
+function mouseClicked(){
+  buttonPressed = true;
 }
 
-function randomQuote(){
-    let quote = random(quotes);
-    textSize(30);
-    fill(0);
-    textFont(arial);
-    textAlign(CENTER);
+function draw() {
+    loadImage('frog1.jpg', img =>{
+        Image(img, width * 0.2, width * 0.26, width * 0.4);
+    });
+
+  if (buttonPressed == true){
+    index =int(random(15));
+    text(frogQuote[index].name,mouseX,mouseY);
+   buttonPressed = false;
+  }
 }
